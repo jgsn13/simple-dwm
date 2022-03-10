af226b21803da7bb800a45a4de5be7845dfe4f09
Joaquim Gregório <joaquim.ibi@gmail.com>
2022-03-09 23:09:01 -0300

    change some defaults

--- config.def.h
+++ config.def.h
@@ -36,9 +36,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
@@ -156,15 +156,15 @@ static Key keys[] = {
     {MODKEY|ShiftMask,                 XK_comma,      tagmon,            {.i = -1}},
     {MODKEY|ShiftMask,                 XK_period,     tagmon,            {.i = +1}},
     {MODKEY,                           XK_n,          toggletag,         {.ui = 1}},
-	{MODKEY,                           XK_s,          show,             {0}},
-	{MODKEY,                           XK_h,          hide,             {0}},
+	{MODKEY,                           XK_s,          show,              {0}},
+	{MODKEY,                           XK_h,          hide,              {0}},
     TAGKEYS(XK_1, 0)
     TAGKEYS(XK_2, 1)
     TAGKEYS(XK_3, 2)
     TAGKEYS(XK_4, 3)
     TAGKEYS(XK_5, 4)
     TAGKEYS(XK_6, 5)
-    {MODKEY | ShiftMask, XK_q, quit, {0}},
+    {MODKEY|ShiftMask,                 XK_q,          quit,              {0}},
 };
 
 /* button definitions */
